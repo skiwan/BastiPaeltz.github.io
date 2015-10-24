dc411924160074667bf564a51da6650a37bbab7f
Sebastian Pältz <bastipaeltz@googlemail.com>
2015-10-24 20:40:03 +0200

    customisation and post-processing

--- post-production.js
+++ post-production.js
@@ -1,4 +1,15 @@
 
 (function () {
+	// remove source link in top right corner
 	$('#sourcelink').remove();
+
+	// center heading for all pages except the blog
+	if (document.location.href.indexOf('blog') < 0){
+		$('h1').addClass('text-center');
+	}
+
+	// style projects page different
+	if (document.title === "Projects | kaderx.com") {
+		$('.section').css('margin-bottom', '70px');
+	}
 })();
